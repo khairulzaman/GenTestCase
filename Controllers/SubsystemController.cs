a483722ad55cbb1640077d41257fba0ea3efe747
Khairulzaman Kamarulzaman <khairulzaman.kamarulzaman@gmail.com>
2014-08-26 15:46:38 +0800

    Added CRUD for Equipment, Subsystem and TestPhase
    Added CRUD for Equipment, Subsystem and TestPhase
    Also changed Web.config for local database to SQLEXPRESS.

---
+++ Controllers/SubsystemController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GenTestCase.Models;
+
+namespace GenTestCase.Controllers
+{
+    public class SubsystemController : Controller
+    {
+        private GTCEntities db = new GTCEntities();
+
+        //
+        // GET: /Subsystem/
+
+        public ActionResult Index()
+        {
+            return View(db.Subsystems.ToList());
+        }
+
+        //
+        // GET: /Subsystem/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Subsystem subsystem = db.Subsystems.Find(id);
+            if (subsystem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subsystem);
+        }
+
+        //
+        // GET: /Subsystem/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Subsystem/Create
+
+        [HttpPost]
+        public ActionResult Create(Subsystem subsystem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Subsystems.Add(subsystem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(subsystem);
+        }
+
+        //
+        // GET: /Subsystem/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Subsystem subsystem = db.Subsystems.Find(id);
+            if (subsystem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subsystem);
+        }
+
+        //
+        // POST: /Subsystem/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Subsystem subsystem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(subsystem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(subsystem);
+        }
+
+        //
+        // GET: /Subsystem/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Subsystem subsystem = db.Subsystems.Find(id);
+            if (subsystem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(subsystem);
+        }
+
+        //
+        // POST: /Subsystem/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Subsystem subsystem = db.Subsystems.Find(id);
+            db.Subsystems.Remove(subsystem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/TestPhaseController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GenTestCase.Models;
+
+namespace GenTestCase.Controllers
+{
+    public class TestPhaseController : Controller
+    {
+        private GTCEntities db = new GTCEntities();
+
+        //
+        // GET: /TestPhase/
+
+        public ActionResult Index()
+        {
+            return View(db.TestPhases.ToList());
+        }
+
+        //
+        // GET: /TestPhase/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            TestPhase testphase = db.TestPhases.Find(id);
+            if (testphase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(testphase);
+        }
+
+        //
+        // GET: /TestPhase/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /TestPhase/Create
+
+        [HttpPost]
+        public ActionResult Create(TestPhase testphase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TestPhases.Add(testphase);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(testphase);
+        }
+
+        //
+        // GET: /TestPhase/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            TestPhase testphase = db.TestPhases.Find(id);
+            if (testphase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(testphase);
+        }
+
+        //
+        // POST: /TestPhase/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(TestPhase testphase)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(testphase).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(testphase);
+        }
+
+        //
+        // GET: /TestPhase/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            TestPhase testphase = db.TestPhases.Find(id);
+            if (testphase == null)
+            {
+                return HttpNotFound();
+            }
+            return View(testphase);
+        }
+
+        //
+        // POST: /TestPhase/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            TestPhase testphase = db.TestPhases.Find(id);
+            db.TestPhases.Remove(testphase);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
